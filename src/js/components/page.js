c8147a57c0e47406017c62a7d9bb1b27072b5992
JoelRamos22 <128657225+JoelRamos22@users.noreply.github.com.>
2024-05-18 00:43:05 -0500

    feat: :sparkles: Inicio del diseño del carrito
    Se inicia el diseño del carrito de la campus Shop

--- src/js/components/page.js
+++ src/js/components/page.js
@@ -32,6 +32,7 @@ export class allPage extends LitElement {
         .aside {
             grid-area: 1 / 1 / 2 / 2;
             padding: 2rem;
+            padding-right: 0; 
             color: var(--color-white);
             height: 100vh;
             position: sticky;
@@ -62,7 +63,15 @@ export class allPage extends LitElement {
             border: none; 
             font-size: 1rem; 
             gap: 1rem;
-            padding: 1rem; 
+            padding: .75em;  
+            cursor: pointer; 
+        }
+        #boton-active {
+            background-color: var(--color-white);
+            color: var(--color-main); 
+            width: 100%;
+            border-top-left-radius: 1rem; 
+            border-bottom-left-radius: 1rem;
         }
         .bi-hand-index-thumb {
             transform: rotateZ(90deg)
@@ -136,7 +145,7 @@ export class allPage extends LitElement {
                     <nav>
                         <ul class="list">
                             <li>
-                                <button class="boton_categoria">
+                                <button class="boton_categoria" id="boton-active">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-handbag-fill" viewBox="0 0 16 16">
                                     <path d="M8 1a2 2 0 0 0-2 2v2H5V3a3 3 0 1 1 6 0v2h-1V3a2 2 0 0 0-2-2M5 5H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11v1.5a.5.5 0 0 1-1 0V5H6v1.5a.5.5 0 0 1-1 0z"/>
                                     </svg>
