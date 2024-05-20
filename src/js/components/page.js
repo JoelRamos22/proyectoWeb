ba4f54c61e0380f9292772a4f7b807f94ec97ec6
JoelRamos22 <128657225+JoelRamos22@users.noreply.github.com.>
2024-05-20 00:17:51 -0500

    feat: :sparkles: Finalizacion de las Funcionalidades
    Se finaliza toda la pagina completamente funcional junto con todos sus eventos
    
    Arreglo a los problemas para eliminar y añadir productos

--- src/js/components/page.js
+++ src/js/components/page.js
@@ -13,18 +13,26 @@ export class allPage extends LitElement {
             ...this.data['camiseta'],
             ...this.data['pantalon']
         ];
+        this.products = []
+        this.total = 0; 
     }
     static properties = {
         data: {typeof: Object},
         categoriaSeleccionada: {typeof: String},
         botonActivo: {typeof: String},
+        products: {typeof: Array},
+        total: {typeof: Number}
     }
     handleClickCategoria(categoria) {
+        const carritoComprado = this.shadowRoot.querySelector("#carrito-comprado")
         const carrito = this.shadowRoot.querySelector(".contenedor-carrito")
         const titulo = this.shadowRoot.querySelector(".titulo")
         const container = this.shadowRoot.querySelector(".container_product")
         const tituloCarrito = this.shadowRoot.querySelector(".titulo-carrito")
         const numerito = this.shadowRoot.querySelector(".numerito")
+        const productos = this.shadowRoot.querySelector("#carrito-productos")
+        productos.style.display =  "flex"
+        carritoComprado.style.display = "none"
         numerito.style.background = "var(--color-white)"
         numerito.style.color = "var(--color-main)"
         tituloCarrito.style.display = "none"
@@ -38,30 +46,108 @@ export class allPage extends LitElement {
             this.categoriaSeleccionada = 'todos los productos';
             this.botonActivo = categoria;
         }
+        this.requestUpdate()
     }
     guardarProducto(event) {
-        const producto = {
-            nombre: event.target.closest('.product').querySelector('.product-description').textContent,
-            precio: parseFloat(event.target.closest('.product').querySelector('.product-price').textContent.replace('$', '')),
-        };
-        console.log('Producto guardado:', producto);
-        this.contador++;
-        this.shadowRoot.querySelector('.numerito').textContent = this.contador;
+        const nombreProducto = event.target.closest('.product').querySelector('.product-description').textContent;
+        const precioProducto = parseFloat(event.target.closest('.product').querySelector('.product-price').textContent.replace('$', ''))
+        const img = event.target.closest('.product').querySelector('.img').src
+        const productoExistente = this.products.find((p) => p.nombre === nombreProducto);
+        if (productoExistente) {
+            productoExistente.cantidad++;
+            productoExistente.precio *= productoExistente.cantidad;
+        } else {
+            const nuevoProducto = {
+                nombre: nombreProducto,
+                precio: precioProducto,
+                cantidad: 1,
+                img: img
+            };
+            this.products.push(nuevoProducto);
+        }
+        console.log(this.products)
+        if (!productoExistente) {
+            this.contador++;
+            this.shadowRoot.querySelector('.numerito').textContent = this.contador;
+        }
+        this.totalCompras()
+        this.requestUpdate()
     } 
-    eliminarProductos() {
+    eliminarProducto(event) {
+        const nombreProducto = event.target.closest('.carrito-producto').querySelector('.product-title').textContent;
+        const index = this.products.findIndex((p) => p.nombre === nombreProducto);
+        const carritoVacio = this.shadowRoot.querySelector(".carrito_vacio")
+        const acciones = this.shadowRoot.querySelector("#carrito-acciones")
+
+        if (index > -1) {
+            this.products.splice(index, 1);
+            console.log('Producto eliminado:', nombreProducto);
+        } else {
+            console.log('Producto no encontrado:', nombreProducto);
+        }
+        if (this.products.length === 0) {
+            carritoVacio.style.display = "flex"
+            acciones.style.display = "none"
+        }
+        this.contador--;
+        this.shadowRoot.querySelector('.numerito').textContent = this.contador;
+        this.totalCompras()
+        this.requestUpdate()
+    }
+    eliminarProductosDeLaVista() {
         const carrito = this.shadowRoot.querySelector(".contenedor-carrito")
         const container = this.shadowRoot.querySelector(".container_product")
         const titulo = this.shadowRoot.querySelector(".titulo")
         const tituloCarrito = this.shadowRoot.querySelector(".titulo-carrito")
         const numerito = this.shadowRoot.querySelector(".numerito")
+        const acciones = this.shadowRoot.querySelector("#carrito-acciones")
+        const carritoVacio = this.shadowRoot.querySelector(".carrito_vacio")
         numerito.style.background = "var(--color-main)"
         numerito.style.color = "var(--color-white)"
         tituloCarrito.style.display = "flex"
         titulo.style.display =  "none"
         container.style.display = "none"
         carrito.style.display = "flex"
-        this.botonActivo = 'carrito'   
+        this.botonActivo = 'carrito'
+        if (this.products.length === 0) {
+            acciones.style.display = "none"
+        } else {
+            acciones.style.display = "flex"
+            carritoVacio.style.display = "none"
 
+        }
+        this.requestUpdate()
+    }
+    vaciarCarrito() {
+        const carritoComprado = this.shadowRoot.querySelector("#carrito-comprado")
+        const carritoVacio = this.shadowRoot.querySelector(".carrito_vacio")
+        const productos = this.shadowRoot.querySelector("#carrito-productos")
+        const acciones = this.shadowRoot.querySelector("#carrito-acciones")
+        carritoComprado.style.display = "none"
+        acciones.style.display = "none"
+        this.shadowRoot.querySelector(".numerito").textContent = "0"
+        this.products = [] 
+        productos.style.display = "none"
+        this.contador = 0
+        carritoVacio.style.display = "flex"
+        this.requestUpdate()
+    }
+    totalCompras() {
+        this.total = this.products.reduce((acc, curr) => acc + curr.precio, 0);
+        this.shadowRoot.querySelector("#total-compras").textContent = `$ ${this.total}`;
+        this.requestUpdate();
+    }
+    graciasPorTuCompra() {
+        const carritoVacio = this.shadowRoot.querySelector(".carrito_vacio")
+        const acciones = this.shadowRoot.querySelector("#carrito-acciones")
+        const carritoComprado = this.shadowRoot.querySelector("#carrito-comprado")
+        carritoVacio.style.display =  "none"
+        acciones.style.display =  "none"
+        carritoComprado.style.display = "flex"
+        this.contador = 0 
+        this.shadowRoot.querySelector(".numerito").textContent = 0
+        this.products = []
+        this.requestUpdate()
     }
     render() {
         return html`
@@ -107,7 +193,7 @@ export class allPage extends LitElement {
                                 </button>
                             </li>
                             <li>
-                                <button class="boton_categoria_cart-fill ${this.botonActivo === 'carrito' ? 'boton-activo' : ''}" @click=${() => this.eliminarProductos()}>
+                                <button class="boton_categoria_cart-fill ${this.botonActivo === 'carrito' ? 'boton-activo' : ''}" @click=${() => this.eliminarProductosDeLaVista()}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
                                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                                     </svg>
@@ -136,28 +222,58 @@ export class allPage extends LitElement {
                     </div>
                     <h2 class="titulo-carrito">Carrito de Compras</h2>
                     <div class="contenedor-carrito">
-                        <p class="carrito_vacio">Tu carrito esta vacio 
+                        <p class="carrito_vacio">Tu carrito esta vacio            
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                             <path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
                             </svg>
                         </p>
                         <div id="carrito-productos" class="carrito-productos disabled">
-                            <!-- Esto se va a completar con el JS -->
+                            ${this.products.map((element) => html`
+                                <div class="carrito-producto">
+                                    <img src="${element.img}" class="carrito-producto-imagen">
+                                    <div class="carrito-producto-titulo">
+                                        <small>Titulo</small>
+                                        <h3 class="product-title">${element.nombre}</h3>
+                                    </div>
+                                    <div class="carrito-producto-cantidad">
+                                        <small>Cantidad</small>
+                                        <h3>${element.cantidad}</h3>
+                                    </div>
+                                    <div class="carrito-producto-precio">   
+                                        <small>Precio</small>
+                                        <h3>$${element.precio}</h3>
+                                    </div>
+                                    <div class="carrito-producto-subtotal">
+                                        <small>Subtotal</small>
+                                        <h3>$${element.precio}</h3>
+                                    </div>
+                                    <button class="carrito-producto-eliminar" @click=${this.eliminarProducto}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                            ` )}
                         </div>
                         <div id="carrito-acciones" class="carrito-acciones disabled">
                             <div class="carrito-acciones-izquierda">
-                                <button id="carrito-acciones-vaciar" class="carrito-acciones-vaciar">Vaciar carrito</button>
+                                <button id="carrito-acciones-vaciar" class="carrito-acciones-vaciar" @click=${() => this.vaciarCarrito()}>Vaciar carrito</button>
                             </div>
                             <div class="carrito-acciones-derecha">
                                 <div class="carrito-acciones-total">
                                     <p>Total:</p>
-                                    <p id="total">$3000</p>
+                                    <p id="total-compras">$</p>
                                 </div>
-                                <button id="carrito-acciones-comprar" class="carrito-acciones-comprar">Comprar ahora</button>
+                                <button id="carrito-acciones-comprar" class="carrito-acciones-comprar" @click=${() => this.graciasPorTuCompra()}>Comprar ahora</button>
                             </div>
                         </div>
-                        <p id="carrito-comprado" class="carrito-comprado disabled">Muchas gracias por tu compra. <i class="bi bi-emoji-laughing"></i></p>
+                        <p id="carrito-comprado" class="carrito-comprado disabled"> Muchas gracias por tu compra. 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-laughing" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                            <path d="M12.331 9.5a1 1 0 0 1 0 1A5 5 0 0 1 8 13a5 5 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5M7 6.5c0 .828-.448 0-1 0s-1 .828-1 0S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 0-1 0s-1 .828-1 0S9.448 5 10 5s1 .672 1 1.5"/>
+                            </svg>
+                        </p>
                     </div>
                 </main>
             </div>
@@ -318,7 +434,9 @@ export class allPage extends LitElement {
             color: var(--color-main);
             font-weight: 500; 
         }
-
+        .carrito-comprado {
+            display: none; 
+        }
         .carrito-productos {
             display: flex;
             flex-direction: column;
@@ -353,7 +471,7 @@ export class allPage extends LitElement {
         }
 
         .carrito-acciones {
-            display: flex;
+            display: none;
             justify-content: space-between;
         }
 
